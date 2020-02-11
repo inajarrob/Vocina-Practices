99ced1ebfd9015eb55d6b33b5f526142c7041805
inajarrob <36012044+inajarrob@users.noreply.github.com>
2020-02-11 10:42:30 +0100

    game embed in game zone

---
+++ public/js/phaser.js
@@ -0,0 +1,31 @@
+const widthD = $("#gameContainer").innerWidth()
+const heightD = $("#gameContainer").innerHeight()
+
+const config = {
+    type: Phaser.AUTO,
+    width: widthD,
+    height: heightD,
+    parent: 'gameContainer',
+    scene: [scene1],
+    input: {
+        keyboard: true,     // Keyboard input configuration. true uses the default configuration and false disables keyboard input.
+        mouse: true,        // Mouse input configuration. true uses the default configuration and false disables mouse input.
+        gamepad: true,      // Gamepad input configuration. true enables gamepad input.
+    },
+    // Physics configuration.
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    
+    scale: {
+        mode: Phaser.Scale.FIT,
+        width: 600,
+        height: 450,
+        zoom: 2
+    }
+}
+
+const game = new Phaser.Game(config)
